217b372d1153e07751076b50d5f28c29e40cc8a1
Marc Obaldo <marcklaser@gmail.com>
2016-04-08 22:44:59 +0800

    Move to single page view

---
+++ Lucent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lucent
+//
+//  Created by Marc on 08/04/2016.
+//  Copyright © 2016 Marc Obaldo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
